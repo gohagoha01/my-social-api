288827495dd1c3a56eaf920e25c27f17a33b7bb8
gohagoha01 <a64956112@gmail.com>
2026-02-28 00:43:57 +0500

    Add superuser logic

--- build.sh
+++ build.sh
@@ -10,3 +10,9 @@ python manage.py collectstatic --no-input
 
 # Базаны жаңарту (Миграция)
 python manage.py migrate
+if [ "$DJANGO_SUPERUSER_USERNAME" ]; then
+  python manage.py createsuperuser \
+    --noinput \
+    --username $DJANGO_SUPERUSER_USERNAME \
+    --email $DJANGO_SUPERUSER_EMAIL || true
+fi
